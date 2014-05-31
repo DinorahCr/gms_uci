f7eb18dbb7002deaf8a5c6a415e00591ca38fc42
dinorahcr <dcarrion@uci.edu>
2014-05-31 03:46:39 -0400

    helpers to modify dates output

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,12 @@
 module EventsHelper
+ 
+  def format_date(time)
+    d = I18n.localize(time, :format => "%A, %B %e %Y")
+    "#{d}"
+  end
+  def format_times(time)
+    t = I18n.localize(time, :format => "%l:%M%p").downcase
+    "#{t}"
+  end
+  
 end
